f1a5b456b23b0b241dee19f9d136bf44553a9739
cos7els <cos7els@gmail.com>
2022-11-01 00:22:56 +0300

    Initial commit

---
+++ src/Connect.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.*;
+
+public class Connect {
+    private static final String url = "jdbc:mysql://127.0.0.1/students_tms";
+    private static final String usr = "cos7els";
+    private static final String pwd = "0123456789";
+
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection(url, usr, pwd);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from students");
+            StringBuilder result = new StringBuilder();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String firstName = rs.getString(2);
+                String lastName = rs.getString(3);
+                String city = rs.getString(4);
+                result.append(String.format("%d, %s %s, %s\n", id, firstName, lastName, city));
+            }
+            System.out.println(result);
+            Files.write(Path.of("students.txt"), result.toString().getBytes());
+            result = new StringBuilder();
+            rs = stmt.executeQuery("select * from cities");
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String city = rs.getNString(2);
+                result.append(String.format("%d, %s\n", id, city));
+            }
+            System.out.println(result);
+            Files.write(Path.of("cities.txt"), result.toString().getBytes());
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
