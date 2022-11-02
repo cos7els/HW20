3e0a845fb6f082ff30cd0d57aee9a4d4fdf1130d
cos7els <cos7els@gmail.com>
2022-11-02 17:15:31 +0300

    Add methods

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Connect work = Connect.getInstance();
         System.out.println(work.getAllStudents());
         System.out.println(work.getAllCities());
         work.addStudent("Irina", "Mazur", "Minsk");
+        Thread.sleep(1000);
         System.out.println(work.getAllStudents());
         System.out.println(work.getAllCities());
+        Thread.sleep(1000);
         work.removeStudent("Irina", "Mazur", "Minsk");
         work.removeCity("Minsk");
+        Thread.sleep(1000);
         System.out.println(work.getAllStudents());
         System.out.println(work.getAllCities());
     }
